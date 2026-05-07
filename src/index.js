f733f2bcae4203cd24190f1e3b11b2d2d190bb7d
Xianos51 <xianos51@protonmail.com>
2026-05-07 15:09:38 +0200

    fix: corrige les chemins CSS et JS pour GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Point d'entrée de l'application
 // Ce fichier est utilisé par Vite pour le build
 
-import './styles.css';
 import { VentilationApp } from './app/VentilationApp.js';
 
 // Initialiser l'application quand le DOM est chargé
